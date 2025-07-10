d745d30e65f0969bcf6cdcddb7b8521fc8574b17
Ali <ha5403905@gmail.com>
2025-07-10 18:07:34 +0500

    data types in ts

---
+++ 1_DataTypes/script.js
@@ -0,0 +1,31 @@
+"use strict";
+// Primitive Types: These are the basic building blocks.
+/*
+Number
+Bigint
+String
+Boolean
+Symbol
+Null
+Undefined
+*/
+// Type Inference
+let count = 5; // inferred as number
+// Type Annotations
+let num = 30;
+// Object Types: Used for more complex structures.
+/*
+Array
+Tuple
+Object
+Enum
+Interface
+Any
+Void
+Never
+Unknown
+*/
+// Type Narrowing
+// Type Alises
+// Union
+// Intersection
