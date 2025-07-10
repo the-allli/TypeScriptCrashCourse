86085adca1868d467ae3576875d568493730ba24
Ali <ha5403905@gmail.com>
2025-07-10 18:09:53 +0500

    type narrowing in ts

---
+++ 1_DataTypes/3_TypeNarrowing/script.ts
@@ -0,0 +1,13 @@
+function printId(id: number | string) {
+  // Type Narrowing
+  // Common ways to narrowing types:
+  // typeof check (for primitives: string, number, boolean, symbol, bigint, undefined, object, function)
+  // instanceof checks (for classes)
+  // in operator (for properties on objects)
+  // Equality checks (==, ===)
+  if (typeof id === "string") {
+    // In this block, TypeScript knows 'id' is a string
+  } else {
+    // In this block, TypeScript knows 'id' is a number
+  }
+}
