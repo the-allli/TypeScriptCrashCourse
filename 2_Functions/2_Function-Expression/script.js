f0cc406e707035da6c6d6d391b90de39e18beccb
Ali <ha5403905@gmail.com>
2025-07-11 16:03:33 +0500

    function expression in ts

---
+++ 2_Functions/2_Function-Expression/script.js
@@ -0,0 +1,5 @@
+"use strict";
+// 2. Function Expression
+const subtract = function (a, b) {
+    return a - b;
+};
---
+++ 2_Functions/2_Function-Expression/script.ts
@@ -0,0 +1,4 @@
+// 2. Function Expression
+const subtract = function (a: number, b: number): number {
+  return a - b;
+};
