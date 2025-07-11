7656bfd76c55da98e0e3698f011c00e3809ea8fd
Ali <ha5403905@gmail.com>
2025-07-11 16:06:29 +0500

    arrow function in ts

---
+++ 2_Functions/3_Arrow-Function/script.js
@@ -0,0 +1,7 @@
+"use strict";
+// 3. Arrow Function
+const multiply = (a, b) => {
+    return a * b;
+};
+// Short form
+const multiply1 = (x, y) => x * y;
---
+++ 2_Functions/3_Arrow-Function/script.ts
@@ -0,0 +1,6 @@
+// 3. Arrow Function
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+// Short form
+const multiply1 = (x: number, y: number): number => x * y;
