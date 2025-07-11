d7696910adf8b6516e04d7fc626c8b1ab09f8f07
Ali <ha5403905@gmail.com>
2025-07-11 16:11:20 +0500

    object method in ts

---
+++ 2_Functions/5_Object-Method/script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Object Method
+const calculator = {
+    add(a, b) {
+        return a + b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+};
+console.log(calculator.add(5, 3));
---
+++ 2_Functions/5_Object-Method/script.ts
@@ -0,0 +1,11 @@
+// Object Method
+const calculator = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+};
+
+console.log(calculator.add(5, 3));
